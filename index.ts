c274f127b338fdd6634fd3ab0d43fe6eef3f4604
Elijah Potter <elijah.potter@24G.com>
2022-10-25 19:51:51 -0400

    initial commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { mockDeep, DeepMockProxy } from "vitest-mock-extended";
+
+type thing = {
+  property: string;
+};
+
+export type MockContext = {
+  prisma: DeepMockProxy<thing>;
+};
+
+export const createMockContext = (): MockContext => {
+  return {
+    prisma: mockDeep<thing>(),
+  };
+};
